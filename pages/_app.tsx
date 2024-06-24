0b755675d5b2cd8555da9fed619cbb032372a2b6
LYW6870 <checkyeonwoo@naver.com>
2024-06-24 20:13:37 +0900

    기본세팅

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css';
+import { Global } from '@emotion/react';
+import { globalStyles } from '../src/commons/styles/globalStyles';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <Component {...pageProps} />
+    </>
+  );
+}
