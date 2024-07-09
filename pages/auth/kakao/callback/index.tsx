c3a2d82ac0149a5dc7072ff3fdf59c58f91e5573
LYW6870 <checkyeonwoo@naver.com>
2024-07-09 17:12:41 +0900

    카카오로그인 엑세스토큰받기까지완료

--- pages/auth/kakao/callback/index.tsx
+++ pages/auth/kakao/callback/index.tsx
@@ -4,10 +4,17 @@ import { useRouter } from 'next/router';
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 
-// 이쪽 설정 해줘야 할 듯
-const LOGIN_MUTATION = gql`
-  mutation Login($code: String!) {
-    login(code: $code) {
+// 1. 사용자가 로그인을 완료하여 인가 코드를 받음
+
+// 2. 받은 인가 코드를 백엔드 서버로 보내기
+// 백엔드 서버는 인가 코드를 사용해서 토큰을 받아야함.
+// 토큰을 받아서 가입유저인지 확인하고
+// 가입안되있으면 가입절차 밟고
+// 가입되어있으면 로그인  + 여기서 토큰만료시간이나 재발급 관련은 나중에 생각하기.
+const KAKAO_LOGIN = gql`
+  mutation KAKAO_LOGIN($code: String!) {
+    KAKAO_LOGIN(code: $code) {
+      # 여러과정을거쳐 반환.. 어떤값들을 반환받을까..
       id
       name
     }
@@ -17,14 +24,22 @@ const LOGIN_MUTATION = gql`
 export default function KakaoCallback() {
   const router = useRouter();
   const { code } = router.query;
-  const [login, { data, loading, error }] = useMutation(LOGIN_MUTATION);
+  // const [updateBoard] = useMutation<
+  //   Pick<IMutation, 'updateBoard'>,
+  //   IMutationUpdateBoardArgs
+  // >(UPDATE_BOARD);
+
+  // const [login, { data, loading, error }] = useMutation(GET_KAKAO_TOKEN);
+  const [login, { data, loading, error }] = useMutation(KAKAO_LOGIN);
 
   console.log('랜더링됨');
 
   useEffect(() => {
     if (code) {
-      login({ variables: { code } });
-      console.log('code: ', code);
+      // login({ variables: { code } });
+      // console.log('code: ', code);
+      const result = login({ variables: { code } });
+      console.log('result: ', result);
     }
   }, [code]);
 
